d79c498531d08953a3dcbf85b35bee0991eb7dfa
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2025-01-24 11:25:38 +0600

    protected profile page with kinde auth

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,6 +1,15 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import React from 'react';
+import { redirect } from 'next/navigation';
 
-const Profile = () => {
+const Profile = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    redirect('/api/auth/login');
+  }
+
   return (
     <div className='container mx-auto text-4xl font-bold py-6 px-8'>
       Welcome to your profile!
